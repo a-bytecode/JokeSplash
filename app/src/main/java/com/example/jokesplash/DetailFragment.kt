c2c33eb9c1ff30ad07dc027fecdab073a01aa63a
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-03-06 20:58:42 +0100

    build: Glide hinzugefügt, animation.xml erstellt. -> DetailFragment.kt, HomeFragment.kt

--- app/src/main/java/com/example/jokesplash/DetailFragment.kt
+++ app/src/main/java/com/example/jokesplash/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -31,7 +32,10 @@ class DetailFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
 
-        binding.refreshButton.setOnClickListener {
+        val animations = AnimationUtils.loadAnimation(requireContext(),R.anim.animation)
+
+        binding.jokesCardView.setOnClickListener {
+            binding.jokesCardView.startAnimation(animations)
             viewModel.geJokes()
         }
 
--- app/src/main/java/com/example/jokesplash/HomeFragment.kt
+++ app/src/main/java/com/example/jokesplash/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.jokesplash.databinding.HomeFragmentBinding
 
 class HomeFragment: Fragment() {
@@ -26,11 +28,20 @@ class HomeFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+        val drawableElements = arrayOf(R.drawable.funny1,R.drawable.funny3)
+
 
         binding.go4Jokes.setOnClickListener {
             findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToDetailFragment())
         }
+
+        Glide
+            .with(requireContext())
+            .load(drawableElements.random())
+            .centerCrop()
+            .placeholder(drawableElements.random())
+            .into(binding.glideCoverHome)
 
 
     }
