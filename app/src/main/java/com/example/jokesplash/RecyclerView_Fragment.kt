99f6b1c5565c5c5a5adc67893f728935ddc3b9f8
Alexander Astrachanzev <alexejastrein@gmail.com>
2023-03-10 18:43:10 +0100

    build: RecyclerView_Fragment.kt erstelt.

---
+++ app/src/main/java/com/example/jokesplash/RecyclerView_Fragment.kt
@@ -0,0 +1,28 @@
+package com.example.jokesplash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jokesplash.databinding.RecyclerviewFragmentBinding
+
+class RecyclerView_Fragment: Fragment() {
+
+    private lateinit var binding: RecyclerviewFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = RecyclerviewFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
